ef9a284a8c5fa4c6e9d0b4c8e0c6cb635aa9e1ce
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-02-07 15:22:06 +0100

    add ball and ball motion

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("f")
+let motionTop = 20
+let motionLeft = 20
+
+function motionBall() {
+    let ball = document.getElementById("ball")
+    let ballPositionTop = parseInt(ball.style.top)
+    let ballPositionLeft = parseInt(ball.style.left)
+    if (ballPositionTop >= 480) {
+        motionTop = -20
+    }
+    if (ballPositionLeft >= 680){
+        motionLeft = -20
+    }
+    if (ballPositionTop <= 0) {
+        motionTop = 20
+    }
+    if (ballPositionLeft <= 0) {
+        motionLeft = 20
+    }
+    ball.style.top = ballPositionTop + motionTop + "px"
+    ball.style.left = ballPositionLeft + motionLeft + "px"
+}
+window.setInterval(motionBall, 200)
