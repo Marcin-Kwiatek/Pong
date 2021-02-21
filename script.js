20f016541b9110361abb4085193722545cbec189
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-02-21 12:18:33 +0100

    add fuction hitRacket

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function motionBall() {
     let ball = document.getElementById("ball")
     let ballPositionTop = parseInt(ball.style.top)
     let ballPositionLeft = parseInt(ball.style.left)
+    hitRacket(ballPositionLeft, ballPositionTop)
     if (ballPositionTop >= 480) {
         motionTop = - motionTop
     }
-    if (ballPositionLeft >= 680){
+    if (ballPositionLeft >= 680) {
         motionLeft = - motionLeft
     }
     if (ballPositionTop <= 0) {
@@ -27,11 +28,18 @@ window.addEventListener("keydown", racketMotion)
 function racketMotion(event) {
     let racket = document.getElementById("racket")
     let racketPosition = parseInt(racket.style.top)
-    if (event.keyCode == '38' && racketPosition>0) {
+    if (event.keyCode == '38' && racketPosition > 0) {
         racket.style.top = racketPosition - 20 + "px"
     }
-    if (event.keyCode == '40' && racketPosition<460) {
+    if (event.keyCode == '40' && racketPosition < 460) {
         racket.style.top = racketPosition + 20 + "px"
+    }
+}
+function hitRacket(ballPositionLeft, ballPositionTop) {
+    let racket = document.getElementById("racket")
+    let racketPosition = parseInt(racket.style.top)
+    if (ballPositionLeft === 50 && ballPositionTop + 20 >= racketPosition && ballPositionTop <= racketPosition + 40) {
+        motionLeft = - motionLeft
     }
 }
 
